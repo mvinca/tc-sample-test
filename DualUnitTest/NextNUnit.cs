d73227c847aaefe1e4ea370eb03926ed57307f81
Michael J. Vinca <mvinca@harris.com>
2017-07-01 10:52:59 -0400

    See what happens when we add another test fixture, will TeamCity detect?

---
+++ DualUnitTest/NextNUnit.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DualUnitTest
+{
+    [TestFixture]
+    public class NextNUnit
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
